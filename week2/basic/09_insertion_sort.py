c91c7874c52b1d21d9cf6a3f85c198f386928088
HyunSeongLee <devhyun05@gmail.com>
2026-03-08 17:16:22 +0900

    08_bubble_sort, 09_insertion_sort 해결

--- week2/basic/09_insertion_sort.py
+++ week2/basic/09_insertion_sort.py
@@ -34,7 +34,16 @@ def insertion_sort(arr):
     """
     n = len(arr)
     
-    # TODO: 두 번째 원소(인덱스 1)부터 시작
+    for i in range(1, n): 
+        key = arr[i]
+        j = i-1 
+
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key 
+
+     # TODO: 두 번째 원소(인덱스 1)부터 시작
     ## 각 원소를 정렬된 부분에 삽입
     ## 현재 원소를 key에 저장    
     ## key를 삽입할 위치 찾기
@@ -44,6 +53,12 @@ def insertion_sort(arr):
     pass
     
     return arr
+          
+
+        
+  
+ 
+
 
 def insertion_sort_with_steps(arr):
     """
@@ -60,6 +75,10 @@ def insertion_sort_with_steps(arr):
         print(f"정렬된 부분: {arr[:i]}")
         
         # TODO: 삽입 위치 찾기 및 이동
+        while j >= 0 and arr[j] > key: 
+            arr[j+1] = arr[j]
+            j -= 1 
+            
         pass
         
         arr[j + 1] = key
