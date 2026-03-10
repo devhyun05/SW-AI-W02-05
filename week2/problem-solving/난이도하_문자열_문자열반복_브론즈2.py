2ebde545589566c415608756e21ce16aacca5f50
HyunSeongLee <devhyun05@gmail.com>
2026-03-10 21:34:55 +0900

    백준에서만 풀고 push 안했던 코드들

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,16 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+
+import sys 
+input = sys.stdin.readline 
+
+n = int(input())
+
+for _ in range(n):
+    repeated_times, s = input().split()
+    new_s = ""
+    
+    for c in s: 
+        new_s += c * int(repeated_times)
+       
+    print(new_s)
--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,21 @@
 # 배열 - 평균은 넘겠지 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/4344
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+for _ in range(n):
+    student_data = list(map(int, input().split()))
+    total_student = student_data[0]
+    grade_avg = sum(student_data[1:]) / total_student 
+    
+    student_over_avg = 0 
+    
+    for i in range(1, len(student_data)):
+        if student_data[i] > grade_avg: 
+            student_over_avg += 1 
+    
+    result = (student_over_avg / total_student) * 100
+    print(f"{result:.3f}%")
