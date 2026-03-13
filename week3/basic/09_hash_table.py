ce59129406ed81358ab051f0e01798179645a213
HyunSeongLee <devhyun05@gmail.com>
2026-03-13 20:12:19 +0900

    09_hash_table 해결

--- week3/basic/09_hash_table.py
+++ week3/basic/09_hash_table.py
@@ -42,11 +42,21 @@ def manage_grades(students):
     """
     # TODO: 평균 점수 계산
     pass
-    
-    
+    average = 0
+    top_score = 0
+    top_student = ""
+
+    for name, score in students.items():
+        average += score 
+        if score > top_score:
+            top_score = score 
+            top_student = name 
+        
+    average /= len(students)
+
     # TODO: 최고 점수 학생 찾기
     pass
-    
+
     return average, top_student, top_score
 
 def find_student_score(students, name):
@@ -62,6 +72,12 @@ def find_student_score(students, name):
     """
     # TODO: students에서 name 찾기
     pass
+    for student_name, score in students.items():
+        if student_name == name: 
+            return score 
+    
+    return None 
+        
 
 # 테스트 케이스
 if __name__ == "__main__":
