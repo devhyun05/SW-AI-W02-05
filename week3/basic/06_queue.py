83b5a47f123c0438b69f4c8b7c2435e216b3273a
HyunSeongLee <devhyun05@gmail.com>
2026-03-13 14:22:11 +0900

    06_queue 해결

--- week3/basic/06_queue.py
+++ week3/basic/06_queue.py
@@ -45,7 +45,12 @@ def process_print_queue(jobs):
     ## 큐에서 작업 꺼내기
     ## 작업 처리 (출력 및 리스트에 추가)
     pass
-    
+
+    while queue:
+        job = queue.popleft()
+        print(f"처리: {job}")
+        processed.append(job)
+  
     return processed
 
 # 테스트 케이스
