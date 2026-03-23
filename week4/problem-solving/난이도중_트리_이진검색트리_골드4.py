6a4711c5a53d988194ad203628d1c29b27bce9b4
HyunSeongLee <devhyun05@gmail.com>
2026-03-23 19:01:56 +0900

    이진검색트리 해결

--- week4/problem-solving/난이도중_트리_이진검색트리_골드4.py
+++ week4/problem-solving/난이도중_트리_이진검색트리_골드4.py
@@ -1,44 +1,61 @@
 # 트리 - 이진 검색 트리 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/5639
 
+# class TreeNode:
+#     def __init__(self, value, left=None, right=None):
+#         self.value = value 
+#         self.left = left 
+#         self.right = right 
 
-import sys
-sys.setrecursionlimit(10**6)
+# def insert(root, new_node):
+#     if not root: 
+#         return TreeNode(new_node)
 
-class TreeNode:
-    def __init__(self, value, left=None, right=None):
-        self.value = value 
-        self.left = left 
-        self.right = right 
-
-def insert(root, new_node):
-    if not root: 
-        return TreeNode(new_node)
-
-    if root.value > new_node: 
-        root.left = insert(root.left, new_node)
-    else:
-        root.right = insert(root.right, new_node)
+#     if root.value > new_node: 
+#         root.left = insert(root.left, new_node)
+#     else:
+#         root.right = insert(root.right, new_node)
     
-    return root 
+#     return root 
 
-def post_order_traversal(root):
-    if not root:
+# root = TreeNode(tree_nodes[0])
+
+# for node in tree_nodes[1:]:
+#     insert(root, node)
+
+# post_order_traversal(root)
+
+# [50,30,24,5,28,45,98,52,60]
+
+# left = [30,24,5,28,45], right = [98,52,60]
+
+# import sys
+# sys.setrecursionlimit(10**6)
+
+preorder = [50,30,24,5,28,45,98,52,60]
+
+# for line in sys.stdin: 
+#     preorder.append(int(line))
+    
+def solve(start, end):
+    if start > end: 
         return 
 
-    post_order_traversal(root.left)
-    post_order_traversal(root.right)
-    print(root.value)
+    root = preorder[start]
 
-tree_nodes = []
+    mid = end + 1 
 
-for line in sys.stdin:
-    tree_nodes.append(int(line))
+    for i in range(start+1, end+1):
+        if preorder[i] > root: 
+            mid = i 
+            break 
+    
+    solve(start+1, mid-1)
+    solve(mid, end)
+    print(root)
 
-root = TreeNode(tree_nodes[0])
+solve(0, len(preorder)-1)
 
-for node in tree_nodes[1:]:
-    insert(root, node)
 
-post_order_traversal(root)
+
 
