f74e27a791004481d00c7942d0c71f50a908a08c
HyunSeongLee <devhyun05@gmail.com>
2026-03-27 17:24:06 +0900

    02_dp_stairs 해결

--- week5/basic/02_dp_stairs.py
+++ week5/basic/02_dp_stairs.py
@@ -47,12 +47,19 @@ def climb_stairs(n):
     """
     # TODO: 특별한 경우 처리
     pass
-    
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1 
     
     # TODO: dp 배열 생성 및 초기화
+    dp = [0] * (n+1)
+    dp[0], dp[1], dp[2] = 0, 1, 2
     pass
     
     # TODO: 작은 문제부터 차례로 계산
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
     pass
     
     return dp[n]
